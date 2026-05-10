7059ba94db0149bac6915c57f2ae0767b955f5dd
shubh3421 <shubhamvijaygangurde@gmail.com>
2026-05-10 12:44:33 +0530

    update

---
+++ lec2.js
@@ -0,0 +1,29 @@
+var city="new york";
+console.log(city);
+
+var city="london";
+console.log(city);
+
+
+let studentName="Alice";
+console.log(studentName);
+
+studentName="Bob2";
+console.log(studentName);
+
+
+/*let studentName2="Bob";
+console.log(studentName2);
+*/
+
+const country="USA";
+const pi = 3.14;
+const appname = "MyApp";
+console.log(country);
+console.log(pi);
+console.log(appname);
+
+const ="india"; // error: assigning value to a constant variable is not allowed
+
+
+
--- test.js
+++ test.js
@@ -3,3 +3,14 @@ console.log('My name is Alice.');
 console.log('I am learning JavaScript.');
 console.log(100);
 console.log(10 + 20);
+
+//Single line comment
+
+/*multi line comment 
+using slash and asterisk
+to write comments in multiple lines
+*/
+
+// javaScripit (dynamic typing language - no need to define type)
+let name = 'Alice'; // string variable
+const age = 30; // constant variable
